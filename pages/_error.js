93b63ca0b956aea3a276f37fa534684824b5a270
bowbowzai <jupeng2015@gmail.com>
2021-09-17 15:59:30 +0800

    error

--- pages/_error.js
+++ pages/_error.js
@@ -1,17 +1,12 @@
-function Error({ statusCode }) {
-  return (
-    <p>
-      {" "}
-      {statusCode
-        ? `An error ${statusCode} occurred on server`
-        : "An error occurred on client"}{" "}
-    </p>
-  );
+import Error from "next/error";
+
+function Page({ statusCode }) {
+  return <Error statusCode={statusCode}> </Error>;
 }
 
-Error.getInitialProps = ({ res, err }) => {
+Page.getInitialProps = ({ res, err }) => {
   const statusCode = res ? res.statusCode : err ? err.statusCode : 404;
   return { statusCode };
 };
 
-export default Error;
+export default Page;
