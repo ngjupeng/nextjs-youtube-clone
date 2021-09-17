f491d9a8dd6fbfd09da063ebcd2efb4379d15ca6
bowbowzai <jupeng2015@gmail.com>
2021-09-17 15:23:36 +0800

    update

--- components/Layout/LayoutVideo.jsx
+++ components/Layout/LayoutVideo.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import NavBar from "../navbar/NavBar";
+import NavBar from "../Navbar/NavBar";
 import SnackbarProvider from "react-simple-snackbar";
 
 export default function Layout({ children }) {
