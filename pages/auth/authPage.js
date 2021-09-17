8ed1cdc2e8f813a5a5724af23981b309acd9f4c0
bowbowzai <jupeng2015@gmail.com>
2021-09-17 15:33:40 +0800

    update

--- pages/auth/authPage.js
+++ pages/auth/authPage.js
@@ -1,8 +1,12 @@
 import Auth from "../../components/Auth/Auth";
+import Head from "next/head";
 
 export default function auth() {
   return (
     <div>
+      <Head>
+        <title>Sign In</title>
+      </Head>
       <Auth />
     </div>
   );
